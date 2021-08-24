ad9e5ee2787e36297c6b87430344198540d6dce6
emranblue <emran.blue.120@gmail.com>
2021-08-24 21:45:17 +0600

    here

--- assignment4/a4q1.m
+++ assignment4/a4q1.m
@@ -1,13 +1,14 @@
 clear all;clc;
 a=0.5;
 b=1.5;
-c=(a+b)/2;
 f=@(x) exp(x)-2-cos(exp(x)-2);
 tol=10^-5;
-fprintf(" a \t b\t c\t |f(c)|\n")
-fprintf("%.3f \t %.3f \t %.3f \t %.7f\n",a,b,c,f(c))
 
-while abs(f(c))>tol
+function Bisection(f,a,b,tol)
+  c=(a+b)/2;
+  fprintf(" a \t b\t c\t |f(c)|\n")
+  fprintf("%.3f \t %.3f \t %.3f \t %.7f\n",a,b,c,f(c))
+  while abs(f(c))>tol
     if f(a)*f(c)>0
         a=c;
     else 
@@ -15,4 +16,9 @@ while abs(f(c))>tol
     end 
     c=(a+b)/2;
     fprintf("%.3f \t %.3f \t %.3f \t %.7f\n",a,b,c,abs(f(c)))
+  end
+
 end
+
+
+Bisection(f,a,b,tol)
--- assignment4/a4q3.m
+++ assignment4/a4q3.m
@@ -1,2 +1,32 @@
 clear all;clc;
-f=@(x) 14*x*exp(x^(x-2))-12*exp(x^(x-2))-7*x^3+20*x^2-26*x+12;
+f=@(x) 14*x*exp((x-2))-12*exp((x-2))-7*x^3+20*x^2-26*x+12;
+x=0:0.1:3;
+y=zeros(size(x));
+for i=1:length(x)
+  y(i)=f(x(i));
+endfor
+plot(x,y)
+
+tol=10^-5;
+
+function y=iter(f,x0)
+  syms x;
+  dydx=diff(sym(f));
+  df=double(subs(dydx,{x},{x0}));
+  y=x0-f(x0)/df;
+endfunction
+
+function NewtonRapson(f,a,tol)
+  counter=1;
+  fprintf("iter \t err ")
+  b=iter(f,a)
+  while abs(b-a)>tol
+    fprintf("%d \t %f \t %f \t %f",counter,a,b,abs(b-a))
+    counter++;
+    a=b
+    b=iter(f,a)
+  endwhile
+endfunction
+
+
+NewtonRapson(f,3,tol)
